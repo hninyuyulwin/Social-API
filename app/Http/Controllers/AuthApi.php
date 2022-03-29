556961c27ea9c2d491e514202e69742c68219c13
Rosie Mary <laltnadi@gmail.com>
2022-03-29 14:45:18 +0630

    4 Install Passport

---
+++ app/Http/Controllers/AuthApi.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthApi extends Controller
+{
+    public function register()
+    {
+        return 'nice';
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\AuthApi;
+use Illuminate\Support\Facades\Route;
+
+
+Route::post('register',[AuthApi::class,'register']);
